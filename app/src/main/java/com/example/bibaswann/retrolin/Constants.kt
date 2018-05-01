ceddf8a3ecae2347c25e9cf55651305c1e02fb79
g0g0l <bibaswann.mail@gmail.com>
2018-05-01 12:26:16 +0530

    Updated build tools

--- app/src/main/java/com/example/bibaswann/retrolin/Constants.kt
+++ app/src/main/java/com/example/bibaswann/retrolin/Constants.kt
@@ -3,6 +3,6 @@ package com.example.bibaswann.retrolin
 class Constants
 {
     companion object {
-        val API_ENDPOINT="https://jsonplaceholder.typicode.com/"
+        const val API_ENDPOINT="https://jsonplaceholder.typicode.com/"
     }
 }
